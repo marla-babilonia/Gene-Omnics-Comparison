e39f3302cb569e65fbb959d93ec5e28866d11017
marla-babilonia <marla.babilonia@upr.edu>
2025-04-04 20:06:48 -0400

    commit all

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask for mst spreadsheet
+
+        // HashMap firstGeneMst = GeneMapsHandler.getMstFromUser("Enter the first file path", scanner);
+        // HashMap secondGeneMst = GeneMapsHandler.getMstFromUser("Enter the second file path", scanner);
+
+        // GeneMapsHandler.printComputedComparison(firstGeneMst, secondGeneMst);
+        // GeneMapsHandler.printMatrix("First MST Matrix: ", firstGeneMst);
+        // GeneMapsHandler.printMatrix("Second MST Matrix: ", secondGeneMst);
+
+        // GeneMapsHandler.printAllToExcel();
+    }
+}
